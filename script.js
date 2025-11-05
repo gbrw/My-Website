733bf8e9401cfe57e067787e254925ab6a731177
Ghaith baraa mohammed <122211246+gbrw@users.noreply.github.com>
2025-11-05 22:24:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,34 +5,6 @@ menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 }
 
-const menuIcon = document.getElementById('menu-icons');
-const navLinks = document.querySelector('.nav-links');
-
-// فتح وغلق المينيو + تغيير الأيقونة
-menuIcon.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-
-    // إذا انفتحت القائمة → خلي X
-    if (navLinks.classList.contains('active')) {
-        menuIcon.classList.remove('fa-bars');
-        menuIcon.classList.add('fa-xmark');
-    } 
-    // إذا انغلقت القائمة → رجّع Bars
-    else {
-        menuIcon.classList.remove('fa-xmark');
-        menuIcon.classList.add('fa-bars');
-    }
-});
-
-// إغلاق عند الضغط على أي رابط + رجوع الأيقونة
-document.querySelector('.nav-links').addEventListener('click', (e) => {
-    if (e.target.tagName === 'A') {
-        navLinks.classList.remove('active');
-        menuIcon.classList.remove('fa-xmark');
-        menuIcon.classList.add('fa-bars');
-    }
-});
-
  function visitGithub() {
     window.open("https://github.com/gbrw", "_blank");
 }
@@ -59,6 +31,7 @@ document.getElementById("submit-btn").addEventListener("click", function(){
 
     window.location.href = `mailto:gaithalrawi99@gmail.com?subject=${subject}&body=${body}`;
 });
+
 
 
 
